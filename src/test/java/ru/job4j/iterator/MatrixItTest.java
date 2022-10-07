7e0d528ef8ea9b04b4e2e9cc1e3e8343f2b80509
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-10-07 13:23:10 +0500

    5.1.1. Итератор для двухмерного массива int[][] [#9539 #349524]

--- src/test/java/ru/job4j/iterator/MatrixItTest.java
+++ src/test/java/ru/job4j/iterator/MatrixItTest.java
@@ -2,7 +2,6 @@ package ru.job4j.iterator;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
