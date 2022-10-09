be897546c8497947334bdc18533229fe94d099ce
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-10-09 18:06:32 +0500

    3. Удалить head в односвязном списке. [#51424]

--- src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
+++ src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
