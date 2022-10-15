ec85e2224d4d19da8debfe158a219a5ece406a2e
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-10-15 11:45:17 +0500

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static void main(String[] args) {
+        try (FileOutputStream outputStream = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    int multi = i * j;
+                    String rsl = "" + i + " * " + j + " = " + multi;
+                    outputStream.write(rsl.getBytes());
+                    outputStream.write(System.lineSeparator().getBytes());
+                }
+                outputStream.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int[][] matrix = new int[10][10];
+        try (FileOutputStream outputStream = new FileOutputStream("result2.txt")) {
+            for (int i = 1; i < matrix.length; i++) {
+                for (int j = 1; j < matrix[i].length; j++) {
+                    int multi = i * j;
+                    String rsl = " " + multi;
+                    outputStream.write(rsl.getBytes());
+                }
+                outputStream.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
