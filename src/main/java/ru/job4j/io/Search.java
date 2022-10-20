3b5f807baed7a261991d20448647c46b8ae3fa5c
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-10-20 17:16:48 +0500

    5. Валидация параметров запуска. [#246865 #353463]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,11 +11,10 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        if (isValidate(args)) {
-            Path start = Paths.get(args[0]);
-            search(start, path -> path.toFile().getName().endsWith(args[1]))
-                    .forEach(System.out::println);
-        }
+        isValidate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1]))
+                .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> predicate) throws IOException {
@@ -23,10 +23,22 @@ public class Search {
         return searchFiles.getPaths();
     }
 
-    private static boolean isValidate(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
+    private static void isValidate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Incorrect number of arguments");
         }
-        return true;
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalStateException(String.format("Not exist %s", file.getAbsolutePath()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalStateException(String.format("Not directory %s", file.getAbsolutePath()));
+        }
+        if (args[1].length() < 2) {
+            throw new IllegalStateException("Value less than 2 characters");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalStateException("Value does not start in dots");
+        }
     }
 }
