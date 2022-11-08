a30687c5d4caaff1e6443341f4f5b1ed9d4f5c34
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-11-08 12:40:07 +0500

    0. Что такое база данных. [#6860 #358855]

--- script.sql
+++ script.sql
@@ -1,12 +1,13 @@
-create table users(
+create table users2(
 	id serial primary key,
 	name varchar(255),
-	description text
+	age integer,
+	text text
 );
-insert into users (name, description) values ('Petr', 'Fouder job4j');
-insert into users (name, description) values ('Maks', 'Student');
-insert into users (name, description) values ('Misha', 'Student');
-select * from users;
-update users set description = 'Stud' where name = 'Maks';
-delete from users where name = 'Misha';
-select * from users;
+insert into users2 (name, age, text) values ('Petr', '43', 'Fouder job4j');
+insert into users2 (name, age, text) values ('Maks', 25, 'Student');
+insert into users2 (name, age, text) values ('Misha', 10, 'Student');
+select * from users2;
+update users2 set age = 30 where name = 'Maks';
+delete from users2 where name = 'Misha';
+select * from users2;
