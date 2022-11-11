3e3d46cf52f0639288ce63b22f1430651a0d8064
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-11-11 17:21:31 +0500

    3. Фильтры [#1732]

---
+++ scripts_test/script_products_request.sql
@@ -0,0 +1,47 @@
+select p.name
+from product p
+inner join type t
+on p.type_id = t.id
+where t.id = 1;
+
+select p.name
+from product p
+where p.name like '%мороженое%';
+
+select p.name, p.expired_date
+from product p
+where current_date > expired_date;
+
+--select p.name, t.name, p.price
+--from product p
+--inner join type t
+--on p.type_id = t.id
+--group by t.name
+
+select p.name, p.price
+from product p
+where p.price = (select max(p.price) from product p);
+
+select t.name, count(p.id) as "Count prod. in type"
+from type t
+inner join product p
+on t.id = p.type_id
+group by t.name;
+
+select p.name
+from product p
+inner join type t
+on p.type_id = t.id
+where t.id = 1 or t.id = 3;
+
+select t.name
+from type t
+inner join product p
+on t.id = p.type_id
+group by t.name
+having count(p.id) < 10;
+
+select p.name, t.name
+from product p
+inner join type t
+on p.type_id = t.id
