6665dd7f6da2c4670b31faac3a77f32249d05caf
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-11-11 08:01:30 +0500

    0. Select [#6861]

---
+++ scripts_test/script_faune.sql
@@ -0,0 +1,18 @@
+create table fauna(
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date) values ('Wolf', 15, '1720-10-01');
+insert into fauna(name, avg_age, discovery_date) values ('Deer', 12, '1970-10-01');
+insert into fauna(name, avg_age, discovery_date) values ('Hare', 4, '1920-10-01');
+insert into fauna(name, avg_age, discovery_date) values ('Raccoon', 7, null);
+insert into fauna(name, avg_age, discovery_date) values ('Kangaroo', 15, '1820-07-01');
+insert into fauna(name, avg_age, discovery_date) values ('Swamp fish blue', 17000, '1820-07-01');
+
+select * from fauna where name like '%fish%';
+select * from fauna where avg_age > 10000 and avg_age < 21000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < '1950-01-01';
