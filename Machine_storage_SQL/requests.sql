f09e5e1e4db703fe4c79e186f9467b8115220b1e
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-11-18 09:41:03 +0500

    Хранилище машин [#1733 #360124]

--- Machine_storage_SQL/requests.sql
+++ Machine_storage_SQL/requests.sql
@@ -6,22 +6,22 @@ on c.body_id = cb.id
 left join car_engines ce
 on c.engine_id = ce.id
 left join car_transmissions ct
-on c.trans_id = ct.id
+on c.trans_id = ct.id;
 
 select cb.name
 from car_bodies cb
 left join cars c
 on cb.id = c.body_id
-where c.body_id is null
+where c.body_id is null;
 
 select ce.name
 from car_engines ce
 left join cars c
 on ce.id = c.engine_id
-where c.engine_id is null
+where c.engine_id is null;
 
 select ct.name
 from car_transmissions ct
 left join cars c
 on ct.id = c.engine_id
-where c.engine_id is null
+where c.engine_id is null;
